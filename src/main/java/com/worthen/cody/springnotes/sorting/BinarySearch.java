19dd2acec7690bab549ef796bfe74f77a390982a
Cody Worthen <cworthen777@hotmail.com>
2022-04-07 12:21:48 -0700

    fixed comments

--- src/main/java/com/worthen/cody/springnotes/sorting/BinarySearch.java
+++ src/main/java/com/worthen/cody/springnotes/sorting/BinarySearch.java
@@ -5,6 +5,11 @@ import java.util.Comparator;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+/**
+ * Finds an element in a range of a sorted array, using the binary search
+ * algorithm. Returns the index where the element is found. Runs in O(log n)
+ * time.
+ */
 @Component
 public class BinarySearch<K> {
 
@@ -15,8 +20,8 @@ public class BinarySearch<K> {
 	}
 
 	/**
-	 * Finds an element in a range of a sorted array, using the binary search
-	 * algorithm. Returns the index where the element is found. O(log n).
+	 * Performs a binary search on an array of elements, depending on the given
+	 * Comparator.
 	 */
 	public int binarySearch(K[] array, K elementToSearchFor, Comparator<K> comp) {
 		return binarySearch(array, 0, array.length - 1, elementToSearchFor, new DefaultComparator<K>());
