f3db78c353e88809d808bb989c140ccba4c3ffa6
Cody Worthen <cworthen777@hotmail.com>
2022-05-04 13:58:47 -0700

    java context unit test

--- src/test/java/com/worthen/cody/springnotes/SpringNotesApplicationTests.java
+++ src/test/java/com/worthen/cody/springnotes/SpringNotesApplicationTests.java
@@ -1,9 +1,11 @@
 package com.worthen.cody.springnotes;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+//@SpringBootTest // comment this out if unit testing with java_context package
+@RunWith(SpringRunner.class)
 public class SpringNotesApplicationTests {
 
 	@Test
---
+++ src/test/java/com/worthen/cody/springnotes/java_context/BinarySearchTest.java
@@ -0,0 +1,43 @@
+package com.worthen.cody.springnotes.java_context;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.worthen.cody.springnotes.SpringSortingApplication;
+import com.worthen.cody.springnotes.sorting.BinarySearch;
+import com.worthen.cody.springnotes.sorting.DefaultComparator;
+
+/**
+ * We want to load the entire Spring context and grab the BinarySearch bean from
+ * that context. (Since we're not using spring-boot-starter-test). Then,
+ * 
+ * 1) call the binarySearch()
+ * 
+ * 2) check if the value is correct
+ */
+
+// LOAD CONTEXT - this is where the context for BinarySearch bean is present:
+@ContextConfiguration(classes = SpringSortingApplication.class)
+// now that the context is provided, we run it (in JUnit 5):
+@ExtendWith(SpringExtension.class)
+public class BinarySearchTest {
+
+	// get the bean from the context by autowiring it in
+	@Autowired
+	private BinarySearch<Integer> binarySearch;
+	private DefaultComparator<Integer> comp = new DefaultComparator<Integer>();
+
+	@Test
+	public void testBasicScenario() {
+		int index = binarySearch.binarySearch(new Integer[] { 3, 1, 2 }, 1, comp);
+
+		// when sorted, array should be [1, 2, 3] putting 1 at index 0
+		assertEquals(0, index);
+	}
+
+}
